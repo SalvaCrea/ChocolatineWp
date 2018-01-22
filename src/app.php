88011a33f61ed2e5772e7f2097c1f8b0a07e11ab
Cardona <hebustha@gmail.com>
2018-01-22 14:12:39 +0100

    ++

--- src/app.php
+++ src/app.php
@@ -1,2 +1,14 @@
 <?php
 namespace WpFramework;
+
+class app{
+    /**
+     * The patch Folder
+     * @var string
+     */
+    public $pathFolder;
+
+    public function __contruct(){
+        $this->pathFolder = get_template_directory();
+    }
+}
