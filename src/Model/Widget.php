16119e1de65f13b7be8b2661afd4812b8479d6c0
Cardona <hebustha@gmail.com>
2018-02-12 13:03:38 +0100

    ++

---
+++ src/Model/Widget.php
@@ -0,0 +1,72 @@
+<?php
+namespace ChocolatineWp\Model;
+
+class Widget extends WP_Widget {
+    /**
+     * Class of widget
+     * @var string
+     */
+    public $className = 'widget-class';
+    /**
+     * Description of widget
+     * @var string
+     */
+    public $desciption = 'description empty...';
+    /**
+     * The unique name
+     * @var string
+     */
+    public $idName = 'uniqName';
+    /**
+     * Description of widget
+     * @var string
+     */
+    public $name = 'Widget Name';
+    /**
+  	 * Sets up the widgets name etc
+  	 */
+  	public function __construct() {
+    		$widget_ops = array(
+    			'classname' => $this->className,
+    			'description' => $this->description,
+    		);
+    		parent::__construct( $this->idName, $this->name, $widget_ops );
+
+        $this->registerWidget();
+  	}
+    public function registerWidget() {
+        \add_action( 'widgets_init', function(){
+            register_widget( __CLASS__ );
+        });
+    }
+  	/**
+  	 * Outputs the content of the widget
+  	 *
+  	 * @param array $args
+  	 * @param array $instance
+  	 */
+  	public function widget( $args, $instance ) {
+  		// outputs the content of the widget
+  	}
+
+  	/**
+  	 * Outputs the options form on admin
+  	 *
+  	 * @param array $instance The widget options
+  	 */
+  	public function form( $instance ) {
+  		// outputs the options form on admin
+  	}
+
+  	/**
+  	 * Processing widget options on save
+  	 *
+  	 * @param array $new_instance The new options
+  	 * @param array $old_instance The previous options
+  	 *
+  	 * @return array
+  	 */
+  	public function update( $new_instance, $old_instance ) {
+  		// processes widget options to be saved
+  	}
+}
